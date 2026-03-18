9d065fba07f7e018c45986be629b09c6ea6d12a5
Rafael Santana <aguiar.rafael003@gmail.com>
2026-03-18 16:31:40 -0300

    Meu primeiro codigo em C++

---
+++ calculo_base.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main () {
+    int numero;
+    std::cout <<"Digite um numero para testar o C++: ";
+    std::cin >>numero;
+
+    std::cout <<"Voce digitou: " << numero << std::endl;
+    return 0;
+}
